7db99b0d032bb78989f34bcf8061c63203b44b39
piso <pisodev0427@gmail.com>
2025-01-07 16:49:59 +0900

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import App from './App';
+import Home from './pages/Home';
+import About from './pages/About';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<React.StrictMode>
+		<Router>
+			<Routes>
+				<Route path='/' element={<App />}>
+					<Route index element={<Home />} />
+					<Route path='about' element={<About />} />
+				</Route>
+			</Routes>
+		</Router>
+	</React.StrictMode>
+);
