d6900d24efcdb6eceacb6ecc77bb51da05ef9cbf
piso <pisodev0427@gmail.com>
2025-01-08 10:49:02 +0900

    feat: create Darkmode

--- src/components/DarkModeToggle.jsx
+++ src/components/DarkModeToggle.jsx
@@ -19,9 +19,28 @@ const DarkModeToggle = () => {
 	}, [darkMode]);
 
 	return (
-		<button onClick={() => setDarkMode(!darkMode)} className='p-2 border rounded'>
-			{darkMode ? 'Switch to Light Mode' : 'Switch to Dark Mode'}
-		</button>
+		<label className='flex items-center cursor-pointer'>
+			<div className='relative'>
+				<input type='checkbox' className='hidden' checked={darkMode} onChange={() => setDarkMode(!darkMode)} />
+				<div className={`block w-14 h-8 rounded-full ${darkMode ? 'bg-green-900' : 'bg-white'}`}></div>
+				<div
+					className={`dot absolute left-1 top-1 w-6 h-6 rounded-full transition ${
+						darkMode ? 'transform translate-x-full bg-rose-50' : 'bg-green-900'
+					}`}></div>
+				<i
+					className={`absolute left-1.5 top-1 transition-opacity ${
+						darkMode ? 'opacity-0' : 'opacity-100'
+					} text-rose-50`}>
+					<i className='bx bx-moon'></i>
+				</i>
+				<i
+					className={`absolute right-1.5 top-1 transition-opacity ${
+						darkMode ? 'opacity-100' : 'opacity-0'
+					} text-green-900`}>
+					<i className='bx bx-sun'></i>
+				</i>
+			</div>
+		</label>
 	);
 };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import DarkModeToggle from '../components/DarkModeToggle';
 function Home() {
 	return (
 		<div className='min-h-screen bg-light-background dark:bg-dark-background text-light-text dark:text-dark-text flex items-center justify-center'>
-			<h1 className='text-3xl font-bold text-center'>Home Page</h1>
+			<h1 className='text-3xl font-bold text-center'>Home Page 홈페이지</h1>
 			<DarkModeToggle />
 		</div>
 	);
